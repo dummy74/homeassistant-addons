762b7aadbc35319f957ea85835cf01dd257bed82
dummy <dummy74@web.de>
2021-01-26 00:30:20 +0100

    testing

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/with-contenv bashio
 
+echo "########################"
 cd /vcontrold/vcontrold/build
 ls -la *
 
+echo "########################"
 lsusb
+echo "########################"
+ls -la /dev/tty*
+echo "########################"
+sleep 10
 
-sleep 5
-
+echo "########################"
 echo "starting vcontrold ..."
 vcontrold -n
 echo "vcontrold finished."

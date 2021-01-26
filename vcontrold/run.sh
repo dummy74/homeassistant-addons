783d1ab90dbef692cf739e9e31ed93db1c26a547
dummy <dummy74@web.de>
2021-01-26 02:15:49 +0100

    ..

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
 echo "########################"
+echo "pwd"
+pwd
+echo "########################"
+echo "cd /vcontrold/vcontrold/build"
 cd /vcontrold/vcontrold/build
 ls -la *
 
@@ -10,14 +14,13 @@ echo "########################"
 echo ">ls -la /dev/tty*"
 ls -la /dev/tty*
 echo "########################"
-echo "USER:"
-echo "$USER"
+for f in $(ls /dev/tty*); do
+	echo ">test -w $f || echo Error"
+	test -w $f || echo Error
+done
 echo "########################"
 echo "GROUPS:"
 echo "groups"
-echo "########################"
-echo ">test -w /dev/ttyUSB0 || echo Error"
-test -w /dev/ttyUSB0 || echo Error
 echo "########################"
 sleep 30
 

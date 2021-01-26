f7f3136cacb27bbc40846cdb1ebedbd1e76be656
dummy <dummy74@web.de>
2021-01-27 00:59:17 +0100

    testing mqtt via mosquitto_pub

--- vcontrold/run.sh
+++ vcontrold/run.sh
@@ -24,9 +24,33 @@ echo "########################"
 echo "GROUPS:"
 groups
 echo "########################"
-sleep 30
+#sleep 10
 
 echo "########################"
 echo "starting vcontrold ..."
-vcontrold -n
-echo "vcontrold finished."
+vcontrold --verbose #--nodaemon
+#echo "vcontrold finished."
+echo "vcontrold daemonized."
+
+
+CONFIG_PATH=/data/options.json
+MQTT_HOST="$(jq --raw-output '.mqtt_host' $CONFIG_PATH)"
+MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)"
+MQTT_PASS="$(jq --raw-output '.mqtt_password' $CONFIG_PATH)"
+INTERVAL="$(jq --raw-output '.interval' $CONFIG_PATH)"
+
+echo "-------------------------------------------"
+echo "Parameters:"
+echo "MQTT Host =" $MQTT_HOST
+echo "Sleep interval =" $INTERVAL
+echo "-------------------------------------------"
+# Enter endless loop
+echo "Enter endless loop ..."
+sleep 10
+while true; do
+	for cmd in "getTempA getTempWW"; do
+		msg=$(vclient -h 127.0.0.1:3002 -c $cmd -k | awk 'BEGIN{FS=":"} {print $2}')
+		/usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i vcontrold -r -t vcontrold/$cmd -m $msg
+	done
+	sleep $INTERVAL
+done
